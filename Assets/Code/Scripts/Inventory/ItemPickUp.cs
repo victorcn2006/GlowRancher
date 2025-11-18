148b357b7d5c4ff1533b10ef951108c829fc84e8
AlanRC9 <alanrb9@gmail.com>
2025-11-18 13:26:42 +0100

    cleanAspiratorThingsOfWorld

--- Assets/Code/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Code/Scripts/Inventory/ItemPickUp.cs
@@ -14,20 +14,4 @@ public class ItemPickUp : MonoBehaviour, IAspirable
     {
         throw new System.NotImplementedException();
     }
-    /*
-private void OnTriggerEnter(Collider other)
-{
-   if (other.CompareTag("Player"))
-   {
-       Inventory inventario = other.GetComponent<Inventory>();
-       if (inventario != null)
-       {
-           // Añadimos el objeto al inventario
-           if (inventario.AñadirAlInventario(icono, nombre))
-           {
-               Destroy(gameObject); // Destruir el objeto en el mundo
-           }
-       }
-   }
-}*/
 }
