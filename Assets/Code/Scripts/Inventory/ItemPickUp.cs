8f6e2e50291e04583a6485f345cf88dd20cae316
AlanRC9 <alanrb9@gmail.com>
2025-11-18 13:23:57 +0100

    aspirator sucktion finished

--- Assets/Code/Scripts/Inventory/ItemPickUp.cs
+++ Assets/Code/Scripts/Inventory/ItemPickUp.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
 
-public class ItemPickUp : MonoBehaviour
+public class ItemPickUp : MonoBehaviour, IAspirable
 {
     public Sprite icono;   // Icono del objeto
     public string nombre;  // Nombre del objeto (debe ser único para que no se confundan)
 
-    private void OnTriggerEnter(Collider other)
+    public void BeingAspired()
     {
-        if (other.CompareTag("Player"))
-        {
-            Inventory inventario = other.GetComponent<Inventory>();
-            if (inventario != null)
-            {
-                // Añadimos el objeto al inventario
-                if (inventario.AñadirAlInventario(icono, nombre))
-                {
-                    Destroy(gameObject); // Destruir el objeto en el mundo
-                }
-            }
-        }
+        Debug.Log("Que me chucla");
     }
+
+    public void StopBeingAspired()
+    {
+        throw new System.NotImplementedException();
+    }
+    /*
+private void OnTriggerEnter(Collider other)
+{
+   if (other.CompareTag("Player"))
+   {
+       Inventory inventario = other.GetComponent<Inventory>();
+       if (inventario != null)
+       {
+           // Añadimos el objeto al inventario
+           if (inventario.AñadirAlInventario(icono, nombre))
+           {
+               Destroy(gameObject); // Destruir el objeto en el mundo
+           }
+       }
+   }
+}*/
 }
