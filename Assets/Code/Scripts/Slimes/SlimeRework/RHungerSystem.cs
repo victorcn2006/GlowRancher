0444e9ee4a7ae6a744e9eaf7154f588509645cac
AlanRC9 <alanrb9@gmail.com>
2025-12-20 19:57:09 +0100

    RSlime Scripts created
    se han creado los scripts para el rework del slime
    se ha agregado el asset de DOTween

--- Assets/Code/Scripts/Slimes/SlimeRework/RHungerSystem.cs
+++ Assets/Code/Scripts/Slimes/SlimeRework/RHungerSystem.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RSlimeMovementBehaviour : MonoBehaviour
+public class RHungerSystem : MonoBehaviour
 {
-
-
-
 
 
 }
--- Assets/Code/Scripts/Slimes/SlimeRework/RSlime.cs
+++ Assets/Code/Scripts/Slimes/SlimeRework/RSlime.cs
@@ -7,10 +7,13 @@ public class RSlime : MonoBehaviour
     // --------------------------------------------LINKED SCRIPTS------------------------------------------------- \\
     // Acabar de crear y agregar todos los RFunction \\
     [Header("SLIME SCRIPTS")]
-    [SerializeField] private RSlimeStateMachine stateMachine ;
-    [SerializeField] private HungerSystem hungerSystem;
-    [SerializeField] private FoodDetector foodDetector;
-    [SerializeField] private GemSystem gemSystem;
-    [SerializeField] private Mouth mouth;
+    [SerializeField] private RSlimeMovementStateMachine stateMachine ;
+    [SerializeField] private RHungerSystem hungerSystem;
+    [SerializeField] private RFoodDetector foodDetector;
+    [SerializeField] private RGemSystem gemSystem;
+    [SerializeField] private RMouth mouth;
     [SerializeField] private RSlimeMovementBehaviour movementbehaviour;
+
+
+
 }
