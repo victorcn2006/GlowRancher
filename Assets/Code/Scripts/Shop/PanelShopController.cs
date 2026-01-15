40a370a78cef867d7e57fedb80385e51fe7a6cba
AP2Gvalls <166146437+AP2Gvalls@users.noreply.github.com>
2026-01-15 19:16:46 +0100

    labels de botiga ja fets
    he fet una nova escena i he fet un nou prefab anomenat Shop, tambe he afegit uns quants sprites Images per a la UI de la botiga.

--- Assets/Code/Scripts/Shop/PanelShopController.cs
+++ Assets/Code/Scripts/Shop/PanelShopController.cs
@@ -9,14 +9,26 @@ public class PanelShopController : MonoBehaviour
     WalletCurrency wallet;
 
     public GameObject incinerator;
+    public GameObject planter;
+    public GameObject SlimeCage;
+    public GameObject Silo;
+    public GameObject Hook;
 
     [Header("Buttons")]
-    [SerializeField]private Button ItemOne;
+    [SerializeField]private Button _itemOne;
+    [SerializeField]private Button _itemPlanter;
+    [SerializeField]private Button _itemSlimeCage;
+    [SerializeField]private Button _itemSilo;
+    [SerializeField]private Button _itemHook;
 
     // Start is called before the first frame update
     void Start()
     {
         incinerator.SetActive(false);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
 
         wallet = WalletCurrency.instance;
     }
@@ -27,9 +39,51 @@ public class PanelShopController : MonoBehaviour
         
     }
 
+    #region Activates
     public void ActivateItemOne() {
         incinerator.SetActive(true);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
     }
+
+    public void ActivatePlanter()
+    {
+        planter.SetActive(true);
+        incinerator.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
+    }
+
+    public void ActivateSlimeCage()
+    {
+        SlimeCage.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
+    }
+
+    public void ActivateSilo()
+    {
+        Silo.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Hook.SetActive(false);
+    }
+
+    public void ActivateHook()
+    {
+        Hook.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+    }
+    #endregion
 
     public void BuyIncinerator() {
 
@@ -41,6 +95,74 @@ public class PanelShopController : MonoBehaviour
 
             Debug.Log("Incinerator purchased");
             incinerator.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuyPlanter()
+    {
+
+        if (wallet.bank >= 5)
+        {
+            wallet.bank -= 5;
+            wallet.SaveMoney();
+            wallet.Score_txt.text = wallet.bank.ToString();
+
+            Debug.Log("Planter purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuySlimeCage()
+    {
+
+        if (wallet.bank >= 5)
+        {
+            wallet.bank -= 5;
+            wallet.SaveMoney();
+            wallet.Score_txt.text = wallet.bank.ToString();
+
+            Debug.Log("Slime Cage purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuySilo()
+    {
+
+        if (wallet.bank >= 5)
+        {
+            wallet.bank -= 5;
+            wallet.SaveMoney();
+            wallet.Score_txt.text = wallet.bank.ToString();
+
+            Debug.Log("Silo purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuyHook()
+    {
+
+        if (wallet.bank >= 5)
+        {
+            wallet.bank -= 5;
+            wallet.SaveMoney();
+            wallet.Score_txt.text = wallet.bank.ToString();
+
+            Debug.Log("Hook purchased");
         }
         else
         {
