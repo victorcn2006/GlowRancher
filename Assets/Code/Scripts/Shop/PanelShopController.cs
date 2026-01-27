2621767a7982b5ca5461d144116775ce7eaf475e
AP2Gvalls <166146437+AP2Gvalls@users.noreply.github.com>
2026-01-27 18:59:03 +0100

    feat:MercadoCompra (#6)
    * labels de botiga ja fets
    
    he fet una nova escena i he fet un nou prefab anomenat Shop, tambe he afegit uns quants sprites Images per a la UI de la botiga.
    
    * comit timidillo
    
    * comit de save
    
    * Commit variacion del mercado
    
    he afegit el segun slot de la rubrica. El mercado varia aleatoriamente, el precio de algunos recursos se ve modificado por su escasez/abundancia.
    
    * fixes
    
    I fixed the issues that were named in the pr
    
    * commit previsiu de canvis
    
    * comit de tot correcte

--- Assets/Code/Scripts/Shop/PanelShopController.cs
+++ Assets/Code/Scripts/Shop/PanelShopController.cs
@@ -6,35 +6,161 @@ using UnityEngine.UI;
 public class PanelShopController : MonoBehaviour
 {
     [Header("Refs")]
-    private WalletCurrency _wallet;
-
-    public GameObject incinerator;
+    [SerializeField] private GameObject incinerator;
+    [SerializeField] private GameObject planter;
+    [SerializeField] private GameObject SlimeCage;
+    [SerializeField] private GameObject Silo;
+    [SerializeField] private GameObject Hook;
 
     [Header("Buttons")]
     [SerializeField] private Button _itemOne;
+    [SerializeField] private Button _itemPlanter;
+    [SerializeField] private Button _itemSlimeCage;
+    [SerializeField] private Button _itemSilo;
+    [SerializeField] private Button _itemHook;
 
     // Start is called before the first frame update
     void Start()
     {
         incinerator.SetActive(false);
-
-        _wallet = WalletCurrency.instance;
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
     }
 
-    public void ActivateItemOne() {
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region Activates
+    public void ActivateItemOne()
+    {
         incinerator.SetActive(true);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
     }
 
-    public void BuyIncinerator() {
+    public void ActivatePlanter()
+    {
+        planter.SetActive(true);
+        incinerator.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
+    }
 
-        if (_wallet.bank >= 5)
+    public void ActivateSlimeCage()
+    {
+        SlimeCage.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        Silo.SetActive(false);
+        Hook.SetActive(false);
+    }
+
+    public void ActivateSilo()
+    {
+        Silo.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Hook.SetActive(false);
+    }
+
+    public void ActivateHook()
+    {
+        Hook.SetActive(true);
+        incinerator.SetActive(false);
+        planter.SetActive(false);
+        SlimeCage.SetActive(false);
+        Silo.SetActive(false);
+    }
+    #endregion
+
+    public void BuyIncinerator()
+    {
+
+        if (WalletCurrency.instance.bank >= 5)
         {
-            _wallet.bank -= 5;
-            _wallet.SaveMoney();
-            _wallet.Score_txt.text = _wallet.bank.ToString();
+            WalletCurrency.instance.bank -= 5;
+            WalletCurrency.instance.SaveMoney();
+            WalletCurrency.instance.Score_txt.text = WalletCurrency.instance.bank.ToString();
 
             Debug.Log("Incinerator purchased");
             incinerator.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuyPlanter()
+    {
+
+        if (WalletCurrency.instance.bank >= 5)
+        {
+            WalletCurrency.instance.bank -= 5;
+            WalletCurrency.instance.SaveMoney();
+            WalletCurrency.instance.Score_txt.text = WalletCurrency.instance.bank.ToString();
+
+            Debug.Log("Planter purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuySlimeCage()
+    {
+
+        if (WalletCurrency.instance.bank >= 5)
+        {
+            WalletCurrency.instance.bank -= 5;
+            WalletCurrency.instance.SaveMoney();
+            WalletCurrency.instance.Score_txt.text = WalletCurrency.instance.bank.ToString();
+
+            Debug.Log("Slime Cage purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuySilo()
+    {
+
+        if (WalletCurrency.instance.bank >= 5)
+        {
+            WalletCurrency.instance.bank -= 5;
+            WalletCurrency.instance.SaveMoney();
+            WalletCurrency.instance.Score_txt.text = WalletCurrency.instance.bank.ToString();
+
+            Debug.Log("Silo purchased");
+        }
+        else
+        {
+            Debug.Log("Not enough money");
+        }
+    }
+
+    public void BuyHook()
+    {
+
+        if (WalletCurrency.instance.bank >= 5)
+        {
+            WalletCurrency.instance.bank -= 5;
+            WalletCurrency.instance.SaveMoney();
+            WalletCurrency.instance.Score_txt.text = WalletCurrency.instance.bank.ToString();
+
+            Debug.Log("Hook purchased");
         }
         else
         {
