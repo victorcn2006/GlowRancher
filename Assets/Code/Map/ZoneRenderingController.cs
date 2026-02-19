c78a3ee85ef09f4254b46ea5fc76bf49fcb30197
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-02-19 16:48:24 +0100

    finishedBaseMap

---
+++ Assets/Code/Map/ZoneRenderingController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneRenderingController : MonoBehaviour
+{
+    
+    [SerializedField] private List<GameObject> zones = new List<GameObject>;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Code/Map/ZoneRenderingTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneRenderingTrigger : MonoBehaviour
+{
+
+    [SerializeField] private List<GameObject> mapZonesToActive = new List<GameObject>();
+
+    private void OnTriggerEnter()
+    {
+        ZoneRenderingController.Instance.SetActiveZones(mapZonesToActive);
+    }
+
+
+}
