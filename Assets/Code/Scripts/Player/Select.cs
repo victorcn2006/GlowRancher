6ec802d5884962ebccc5c2e95a18814467b0db6a
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-02-20 20:49:05 +0100

    Last one

--- Assets/Code/Scripts/Player/Select.cs
+++ Assets/Code/Scripts/Player/Select.cs
@@ -5,50 +5,59 @@ using UnityEngine.InputSystem;
 
 public class Select : MonoBehaviour
 {
-    private LightInteractionController _lightContrll;
-    private InteractiveShop _interactiveShop;
+    [Header("Configuración de Raycast")]
+    [SerializeField] private float _distance = 1.5f;
+    [SerializeField] private LayerMask _interactMask;
 
-    private LayerMask _mask;
-    public float distance = 1.5f;
+    [Header("Referencias")]
+    [SerializeField] private Transform _raycastOrigin;
 
-    [SerializeField] private InputAction _interact;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        _mask = LayerMask.GetMask("Raycast layer");
-    }
-
-    // Update is called once per frame
-    private void InteractFilter()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, _mask)) 
+        if(_raycastOrigin == null && Camera.main != null)
         {
-            if (hit.collider.tag == "InteractuableObject" ) 
-            {
-                hit.collider.transform.GetComponent<LightInteractionController>().ActivateObject();
-            }
-
-            if (hit.collider.tag == "InteractuableShop")
-            {
-                Debug.Log("Shop.Entra");
-                hit.collider.transform.GetComponent<InteractiveShop>().ToggleShop(); // Canviem aquí
-            }
-            else
-            {
-                Debug.Log("Error");
-            }
+            _raycastOrigin = Camera.main.transform;
         }
     }
 
-    public void Interact(InputAction.CallbackContext ctx)
+    public void OnInteract(InputAction.CallbackContext ctx)
     {
         if (ctx.performed)
         {
-            InteractFilter();
+            PerformSelection();
         }
     }
+
+    private void PerformSelection()
+    {
+        Vector3 origin = _raycastOrigin != null ? _raycastOrigin.position : transform.position;
+        Vector3 direction = _raycastOrigin != null ? _raycastOrigin.forward : transform.forward;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(origin, direction, out hit, _distance, _interactMask))
+        {
+            if (hit.collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactable.Interact();
+                Debug.Log($"<color=cyan>[Interacción]</color> Ejecutada en: {hit.collider.name}");
+            }
+            else
+            {
+                Debug.LogWarning($"El objeto '{hit.collider.name}' está en la capa interactuable pero no tiene el componente IInteractable.");
+            }
+        }
+    }
+    #region Debug y Visualización
+    private void OnDrawGizmos()
+    {
+        if (_raycastOrigin == null && Camera.main != null) _raycastOrigin = Camera.main.transform;
+
+        if (_raycastOrigin != null)
+        {
+            Gizmos.color  = Color.yellow;
+            Gizmos.DrawRay(_raycastOrigin.position, _raycastOrigin.forward * _distance);
+        }
+    }
+    #endregion
 }
