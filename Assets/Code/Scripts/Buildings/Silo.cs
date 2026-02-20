cae8f2d650bafa26703c08be1d47b76445175fc7
unknown <Victor Carrillo>
2026-02-20 20:51:10 +0100

    Add the silo building

--- Assets/Code/Scripts/Buildings/Silo.cs
+++ Assets/Code/Scripts/Buildings/Silo.cs
@@ -1,12 +1,37 @@
-
-using System;
 using UnityEngine;
 
 public class Silo : Building
 {
     [SerializeField] private GameObject _inventoryPanel;
 
+    private bool _playerInside;
+
+
     protected override void Awake() {
         position = this.transform.position;
+    }
+
+    private void OnEnable()
+    {
+        InputManager.Instance.OnInteract.AddListener(ActiveInventoryPanel);
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnInteract.RemoveListener(ActiveInventoryPanel);
+    }
+
+    public void SetPlayerInside(bool value)
+    {
+        _playerInside = value;
+
+        if (!value)
+            _inventoryPanel.SetActive(false);
+    }
+
+    private void ActiveInventoryPanel()
+    {
+        if (_playerInside)
+            _inventoryPanel.SetActive(true);
     }
 }
