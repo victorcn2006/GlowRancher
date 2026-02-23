98e31b93bf8466a37570b744ed79a056deeea0fe
AP2Gvalls <166146437+AP2Gvalls@users.noreply.github.com>
2026-02-23 20:18:59 +0100

    Ft translator (#18)
    * stash 1
    
    * ft-game_Options_translated
    
    * ft-AudioPanel_Translated
    
    * ft-graphicsPanel_Translated
    
    * ft-options_translated
    
    * ft-MainMenu_Translated
    
    * ft-slimepedia_translated
    
    * ft-shop_Translated
    
    * ft-credits_translated
    
    * comit preventiu
    
    * Funciona
    
    * comit de respaldo

--- Assets/Code/Scripts/UICarrousel.cs
+++ Assets/Code/Scripts/UICarrousel.cs
@@ -2,8 +2,6 @@ using TMPro;
 using UnityEngine;
 
 using System.Collections;
-using TMPro;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class UICarrousel : MonoBehaviour
@@ -19,8 +17,8 @@ public class UICarrousel : MonoBehaviour
     [Header("UI")]
     [SerializeField] private CarouselItem[] _items;
     [SerializeField] private TextMeshProUGUI _titleText;
-    [SerializeField] private Button _prevButton;
-    [SerializeField] private Button _nextButton;
+     public Button _prevButton;
+     public Button _nextButton;
 
     [Header("Transition Settings")]
     [SerializeField] private float _transitionDuration = 0.5f;
@@ -37,6 +35,14 @@ public class UICarrousel : MonoBehaviour
         Scale
     }
 
+    public string GetValue()
+    {
+        if (_items.Length > 0 && _currentIndex >= 0 && _currentIndex < _items.Length)
+        {
+            return _items[_currentIndex].title;
+        }
+        return null;
+    }
     private void Start()
     {
         _prevButton.onClick.AddListener(Previous);
