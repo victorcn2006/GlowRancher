bbf36e83ef2067cbdb4729899d6468098807ad7b
Pau-max <a24pauceppor@inspedralbes.cat>
2026-03-16 20:21:55 +0100

    LogicoadingScreen

---
+++ Assets/Code/Scripts/LoadingScreen/LoadSceneName.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneName : MonoBehaviour
+{
+    public void ScenaMainMenu()
+    {
+        LoaderScene.Instance.LoadScene(ConstantGame.MAINMENU);
+    }
+
+    public void ScenaWorld()
+    {
+        LoaderScene.Instance.LoadScene(ConstantGame.WORLD);
+    }
+
+    public void ScenaCredit()
+    {
+        LoaderScene.Instance.LoadScene(ConstantGame.CREDITSCENE);
+    }
+
+    public void ScenaOptions()
+    {
+        LoaderScene.Instance.LoadScene(ConstantGame.OPTIONSMENU);
+    }
+}
