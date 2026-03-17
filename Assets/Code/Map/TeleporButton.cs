19d1c22461db0e333fe411eabba791b6cedb68b8
Pau-max <a24pauceppor@inspedralbes.cat>
2026-03-17 20:09:43 +0100

    Tp

--- Assets/Code/Map/TeleporButton.cs
+++ Assets/Code/Map/TeleporButton.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 
+
+
 public class TeleporButton : MonoBehaviour
 {
     [SerializeField] private GameObject _map;
     [SerializeField] private InteractiveMap _update;
     [SerializeField] private GameObject _player;
     [SerializeField] private Transform _teleport;
+    [SerializeField] private Transform _moveTp;
     [SerializeField] private float _delayTime = 2.0f;
     Coroutine _coroutine;
-   
 
     public void OnButtonClick()
     {
-        // IMPORTANTE: La lógica debe estar DENTRO de la corrutina
         if (_coroutine == null)
         {
             _coroutine = GameManager.Instance.StartCoroutine(TeleportSequence());
@@ -28,40 +29,29 @@ public class TeleporButton : MonoBehaviour
         yield return new WaitForSecondsRealtime(_delayTime);
         Teleport();
         UpdateGame();
-
-
-;
     }
-
     public void Teleport()
     {
         if (_player != null && _teleport != null)
         {
-
             // Mueve la posición
             _player.transform.position = _teleport.position;
-
             // Aplica la rotación completa (es la forma correcta)
             _player.transform.rotation = _teleport.rotation;
         }
     }
-
     private void UpdateGame()
     {
         if (_update != null)
         {
-            ;
             _update.UpdateGameState(false);
         }
         _coroutine = null;
     }
-
     private void DoAnimationRotate()
     {
         if (_player != null && _teleport != null)
         {
-            // Usamos DORotate para que la rotación del jugador coincida 
-            // exactamente con la rotación del objeto destino (_teleport)
             _player.transform.DORotate(_teleport.eulerAngles, _delayTime)
                 .SetEase(Ease.InOutSine)
                 .SetUpdate(UpdateType.Normal, true);
