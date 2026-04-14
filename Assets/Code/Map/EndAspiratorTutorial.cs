d969b3827967953f50cb150781c225dc43028e10
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-04-14 20:08:25 +0200

    tutoFinished

---
+++ Assets/Code/Map/EndAspiratorTutorial.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAspiratorTutorial : MonoBehaviour
+{
+
+    [SerializeField] GameObject _lightGo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            DeleteObject(_lightGo);
+        }
+    }
+
+    public void DeleteObject(GameObject obj) => Destroy(obj);
+}
