9ee7ca06ea3221f89bea9e25522f0bf22f38c1ef
AP2Gvalls <166146437+AP2Gvalls@users.noreply.github.com>
2026-04-15 19:03:17 +0200

    Feat/fix shop (#86)
    Co-Authored-By: a24pausanmun-collab <a24pausanmun@inspedralbes.cat>

--- Assets/Code/Scripts/WikiSLime/WikiSlime.cs
+++ Assets/Code/Scripts/WikiSLime/WikiSlime.cs
@@ -6,8 +6,18 @@ public class WikiSlime : MonoBehaviour
     [SerializeField] private GameObject _wikiMenu;
     [SerializeField] private GameObject _slime1;
     [SerializeField] private GameObject _slime2;
+    [SerializeField] private GameObject _slime3;
+    [SerializeField] private GameObject _slime4;
+    [SerializeField] private GameObject _slime5;
+    [SerializeField] private GameObject _slime6;
+    [SerializeField] private GameObject _slime7;
     [SerializeField] private Button _slime1Button;
     [SerializeField] private Button _slime2Button;
+    [SerializeField] private Button _slime3Button;
+    [SerializeField] private Button _slime4Button;
+    [SerializeField] private Button _slime5Button;
+    [SerializeField] private Button _slime6Button;
+    [SerializeField] private Button _slime7Button;
 
     private GameObject _currentActiveSlime;
 
@@ -15,11 +25,16 @@ public class WikiSlime : MonoBehaviour
     {
 
         _currentActiveSlime = _slime1;
-        _slime1.SetActive(true);
+        _slime1.SetActive(false);
         _slime2.SetActive(false);
 
         _slime1Button.onClick.AddListener(() => SwitchSlime(_slime1));
         _slime2Button.onClick.AddListener(() => SwitchSlime(_slime2));
+        _slime3Button.onClick.AddListener(() => SwitchSlime(_slime3));
+        _slime4Button.onClick.AddListener(() => SwitchSlime(_slime4));
+        _slime5Button.onClick.AddListener(() => SwitchSlime(_slime5));
+        _slime6Button.onClick.AddListener(() => SwitchSlime(_slime6));
+        _slime7Button.onClick.AddListener(() => SwitchSlime(_slime7));
     }
 
     public void ActiveWiki()
