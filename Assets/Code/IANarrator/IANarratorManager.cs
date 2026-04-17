458627726ad28d38ad79ebd9cd3ca86c0302f31e
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-04-17 19:13:45 +0200

    IANarrator package

--- Assets/Code/IANarrator/IANarratorManager.cs
+++ Assets/Code/IANarrator/IANarratorManager.cs
@@ -8,19 +8,21 @@ public class IANarratorManager : MonoBehaviour
     public static IANarratorManager Instance => instance;
 
     [SerializeField] private GameObject _dialogueGO;
+    [SerializeField] private Dialogue _OnboardingDialogue;
+
+    private List<Dialogue> _dialoguesList = new List<Dialogue>();
     private Dialogue _currentDialogue;
     private int _currentLineIndex;
     private bool _dialogDisplaying;
     private bool _dialogLineFinished;
     private IATextNarrator _iATextNarrator;
-    public Dialogue text;
 
     private void OnEnable() { if (InputManager.Instance != null) InputManager.Instance.OnEnterPerformed.AddListener(NextLineDialog); }
     private void OnDisable() { if (InputManager.Instance != null) InputManager.Instance.OnEnterPerformed.RemoveListener(NextLineDialog); }
 
     private void Start()
     {
-        StartNewDialog(text);
+        StartNewDialog(_OnboardingDialogue);
     }
     void Awake()
     {
@@ -29,7 +31,17 @@ public class IANarratorManager : MonoBehaviour
         _iATextNarrator = GetComponent<IATextNarrator>();
     }
 
-    public void StartNewDialog(Dialogue newDialog)
+    public void AddNewDialogueToQueue(Dialogue newDialogue)
+    {
+        _dialoguesList.Add(newDialogue);
+        if (!_dialogDisplaying)
+        {
+            NextDialogue();
+
+        }
+    }
+    public void RemoveOnListDialogue(Dialogue newDialogue) => _dialoguesList.Remove(newDialogue);
+    private void StartNewDialog(Dialogue newDialog)
     {
         _dialogueGO.SetActive(true);
         _currentDialogue = newDialog;
@@ -45,10 +57,32 @@ public class IANarratorManager : MonoBehaviour
         if (_dialogDisplaying && _dialogLineFinished)
         {
             _currentLineIndex++;
-            if (_currentLineIndex >= _currentDialogue.lines.Count) { _dialogueGO.SetActive(false); _dialogDisplaying = false; }
-            else { _dialogLineFinished = false; StartCoroutine(_iATextNarrator.NarrateLine(_currentDialogue.lines[_currentLineIndex])); }
+            if (_currentLineIndex >= _currentDialogue.lines.Count) //si ha acabado el dialogo
+            {
+                _dialogueGO.SetActive(false); _dialogDisplaying = false;
+                RemoveOnListDialogue(_currentDialogue);
+                NextDialogue();
+            }
+            else
+            {
+                _dialogLineFinished = false; StartCoroutine(_iATextNarrator.NarrateLine(_currentDialogue.lines[_currentLineIndex]));
+            }
+            
         }
     }
 
-    public void DialogueLineFinished() => _dialogLineFinished = true;
+    public void DialogueLineFinished()
+    {
+        _dialogLineFinished = true;
+
+    } 
+
+    private void NextDialogue()
+    {
+        if (_dialoguesList.Count > 0)
+        {
+            StartNewDialog(_dialoguesList[0]);
+        }
+    }
+
 }
