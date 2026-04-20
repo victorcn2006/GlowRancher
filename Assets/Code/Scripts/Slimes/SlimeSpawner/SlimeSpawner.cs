b4ea8f533f38e17fffe55bd5f24efc0b0a89f891
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-04-20 20:50:54 +0200

    a

--- Assets/Code/Scripts/Slimes/SlimeSpawner/SlimeSpawner.cs
+++ Assets/Code/Scripts/Slimes/SlimeSpawner/SlimeSpawner.cs
@@ -9,6 +9,10 @@ public class SlimeSpawner : MonoBehaviour
     [SerializeField] private float _launchRange;
     [SerializeField] private float _launchUpForce;
 
+    [SerializeField] private List<GameObject> slimesList = new List<GameObject>();
+
+    private bool _corrupted = true;
+
     void Start()
     {
         StartCoroutine(LaunchSlimes());
@@ -18,10 +22,33 @@ public class SlimeSpawner : MonoBehaviour
     {
         for (int i = 0; i < _slimesQuantity; i++)
         {
-            yield return new WaitForSeconds(0.5f);
-            GameObject slime = PoolManager.Instance.GetFirstAvailableObject("BasicSlime");
+            GameObject slime;
+            if (_corrupted)
+            {
+                int corruptedProbability = Random.Range(0, 3); //hay una probabilidad de 1 entre 3 de que salga corrupto si el spawner está corrupto
+
+                if (corruptedProbability == 0) slime = PoolManager.Instance.GetFirstAvailableObject("Corrupted");
+                else
+                {
+                    int selectedSlimeTypeIndex = Random.Range(0, slimesList.Count);
+                    slime = PoolManager.Instance.GetFirstAvailableObject(slimesList[selectedSlimeTypeIndex].tag);
+                }
+
+            }
+            else
+            {
+                int selectedSlimeTypeIndex = Random.Range(0, slimesList.Count);
+                slime = PoolManager.Instance.GetFirstAvailableObject(slimesList[selectedSlimeTypeIndex].tag);
+
+            }
+
             slime.transform.position = transform.position;
             slime.GetComponentInChildren<Rigidbody>().AddForce(new Vector3(Random.Range(-_launchRange, _launchRange), _launchUpForce, Random.Range(-_launchRange, _launchRange)), ForceMode.Impulse);
+
+            yield return new WaitForSeconds(0.5f);
         }
     }
+
+    public void SetCorrupted() => _corrupted = false;
+
 }
