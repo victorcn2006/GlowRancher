73b4b032266a35178b138c25ce9c95a2900626dd
Alan Rodríguez <81470568+AlanRC9@users.noreply.github.com>
2026-05-04 17:14:12 +0200

    Ft map 2 (#111)
    * IANarrator package
    
    * update
    
    * a
    
    * DialogsInplemented
    
    * tutoFinished
    
    * puzzlesUpload
    
    * full map update
    
    * WORLD FIX
    
    * Change player tag comparison to 'Player'
    
    * a
    
    * Entry and terrain magic forest
    
    * TutoHecho
    
    * Trees Magic Forest
    
    * Decoration Magic Forest 1.0
    
    * Bosque Magico Avanzado
    
    * Decoration Magic Forest 2.0
    
    * Bosque magico avanzado ++
    
    * Bosque Magico Avanzado +++
    
    * Totem, idle cristales y aspiradora
    
    * Avanazado
    
    * Duplicado_mapa
    
    * Bosque Magico decorado ++++
    
    * Commiteada con el aleix
    
    * Modelos implementados
    
    * BosqueMagicoParteSetas
    
    * commit antes de coger mapa "final"
    
    * SlimeSpawn script finished
    
    ---------
    
    Co-authored-by: victor2006v <161024485+victor2006v@users.noreply.github.com>
    Co-authored-by: PiKa33456 <a24alepiclop@inspedralbes.cat>
    Co-authored-by: a24pausanmun-collab <a24pausanmun@inspedralbes.cat>

--- Assets/Code/Scripts/Slimes/SlimeSpawner/SlimeSpawner.cs
+++ Assets/Code/Scripts/Slimes/SlimeSpawner/SlimeSpawner.cs
@@ -4,24 +4,98 @@ using UnityEngine;
 
 public class SlimeSpawner : MonoBehaviour
 {
-
     [SerializeField] private int _slimesQuantity;
     [SerializeField] private float _launchRange;
     [SerializeField] private float _launchUpForce;
+    [SerializeField] private List<GameObject> slimesList = new List<GameObject>();
+
+    private bool _corrupted = true;
+    private bool _playerInZone = false;
+    private bool _slimesDespawnInProcess;
+
+    private Coroutine _despawnCoroutine;
 
     void Start()
     {
-        StartCoroutine(LaunchSlimes());
+        //StartCoroutine(LaunchSlimes());
     }
 
     private IEnumerator LaunchSlimes()
     {
         for (int i = 0; i < _slimesQuantity; i++)
         {
-            yield return new WaitForSeconds(0.5f);
-            GameObject slime = PoolManager.Instance.GetFirstAvailableObject("BasicSlime");
+            GameObject slime;
+            if (_corrupted)
+            {
+                int corruptedProbability = Random.Range(0, 3); //hay una probabilidad de 1 entre 3 de que salga corrupto si el spawner está corrupto
+                if (corruptedProbability == 0) slime = PoolManager.Instance.GetFirstAvailableObject("CorruptSlime");
+                else
+                {
+                    int selectedSlimeTypeIndex = Random.Range(0, slimesList.Count);
+                    slime = PoolManager.Instance.GetFirstAvailableObject(slimesList[selectedSlimeTypeIndex].name);
+                }
+            }
+            else
+            {
+                int selectedSlimeTypeIndex = Random.Range(0, slimesList.Count);
+                slime = PoolManager.Instance.GetFirstAvailableObject(slimesList[selectedSlimeTypeIndex].name);
+            }
+            slime.SetActive(true);
+            slime.transform.SetParent(transform);
             slime.transform.position = transform.position;
             slime.GetComponentInChildren<Rigidbody>().AddForce(new Vector3(Random.Range(-_launchRange, _launchRange), _launchUpForce, Random.Range(-_launchRange, _launchRange)), ForceMode.Impulse);
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    public void DespawnSlimes()
+    {
+        foreach (Transform child in transform)
+        {
+            //llamar al codigo de victor para activar shader de despawn
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    public void SetCorrupted() => _corrupted = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInZone = true;
+
+            if (_despawnCoroutine != null)
+            {
+                StopCoroutine(_despawnCoroutine);
+                _despawnCoroutine = null;
+            }
+
+            if (!_slimesDespawnInProcess)
+            {
+                StartCoroutine(LaunchSlimes());
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerInZone = false;
+            _slimesDespawnInProcess = true;
+            _despawnCoroutine = StartCoroutine(DespawnSlimesExtraTime());
+        }
+    }
+
+    private IEnumerator DespawnSlimesExtraTime()
+    {
+        yield return new WaitForSeconds(5f);
+        if (!_playerInZone)
+        {
+            DespawnSlimes();
+            _slimesDespawnInProcess = false;
+            _despawnCoroutine = null;
         }
     }
 }
