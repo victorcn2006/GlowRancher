03a0910627d60c22c63d5c1a03b2c6ae6ab2d8a4
Victor Carrillo Navarro <vcarillnav@gmail.com>
2026-05-04 17:09:00 +0200

    Feat: Add gamepad and keyboard icons for rebindings, fix UI layouts to be responsive and add brightness postprocess to the cameras (#110)
    * UI Fixes and Carrousel refactor
    
    * Add all the rebinding icons for gamepad and keyboard
    
    ---------
    
    Co-authored-by: Victor Carrillo <a22viccarnav@inspedralbes.cat>

---
+++ Assets/Code/Scripts/Menu/Settings/BrightnessSlider.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class BrightnessSlider : MonoBehaviour
+{
+    private Slider _slider;
+
+    private void Awake()
+    {
+        _slider = GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        if (BrightnessManager.instance != null)
+        {
+            float val = BrightnessManager.instance.GetBrightness();
+            _slider.SetValueWithoutNotify(val);
+            Debug.Log($"[BrightnessSlider] Initialized with value: {val}");
+        }
+        else
+        {
+            Debug.LogError("[BrightnessSlider] BrightnessManager.instance is NULL! Make sure the manager exists in the first scene.");
+        }
+        
+    }
+
+    public void OnSliderValueChanged(float value)
+    {
+        Debug.Log($"[BrightnessSlider] Slider moved to: {value}");
+        if (BrightnessManager.instance != null)
+        {
+            BrightnessManager.instance.SetBrightness(value);
+        }
+    }
+}
