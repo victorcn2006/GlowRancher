886ab4023ea511d87c855fda356d858341b67222
Victor Carrillo <a22viccarnav@inspedralbes.cat>
2026-05-05 12:55:49 +0200

    Add the animation

--- Assets/Code/Scripts/Ship/WinCondition.cs
+++ Assets/Code/Scripts/Ship/WinCondition.cs
@@ -1,13 +1,31 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 public class WinCondition : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector _timeline;
+
+    private bool _triggered = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            GameManager.Instance.SaveStats();
-            SceneManager.LoadScene("Stats");
-        }
+        if (!other.CompareTag("Player") || _triggered)
+            return;
+
+        _triggered = true;
+
+        StartCoroutine(WinSequence());
+    }
+
+    private IEnumerator WinSequence()
+    {
+        GameManager.Instance.SaveStats();
+        // lanzar timeline
+        _timeline.Play();
+
+        // esperar hasta que termine
+        yield return new WaitWhile(() => _timeline.state == PlayState.Playing);
+
+        SceneManager.LoadScene("Stats");
     }
 }
