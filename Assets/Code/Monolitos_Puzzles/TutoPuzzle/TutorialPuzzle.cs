08c6588970bf583492a5a8740c774927038159c3
Pau-max <a24pauceppor@inspedralbes.cat>
2026-05-11 12:38:20 +0200

    Ft/animation monolito (#156)
    * Add Animation Monolito
    
    * AddAnimationFinish
    
    * Fix
    
    * Fix
    
    ---------
    
    Co-authored-by: Pau <paucepeda06@gmail.com>

--- Assets/Code/Monolitos_Puzzles/TutoPuzzle/TutorialPuzzle.cs
+++ Assets/Code/Monolitos_Puzzles/TutoPuzzle/TutorialPuzzle.cs
@@ -7,5 +7,14 @@ public class TutorialPuzzle : MonoBehaviour
 
     [SerializeField] private Dialogue _dialogue;
 
-    public void FinishPuzzle() => IANarratorManager.Instance.AddNewDialogueToQueue(_dialogue);
+    public void FinishPuzzle()
+    {
+        StartCoroutine(WaitToAnimation());
+        IANarratorManager.Instance.AddNewDialogueToQueue(_dialogue);
+    }
+
+    private IEnumerator WaitToAnimation()
+    {
+        yield return new WaitForSeconds(37f);
+    }
 }
